e2b9be5cd116e8801eceb9e64c8b2f966a4a2d1a
Yamuna <yamubm@gmail.com>
2022-03-14 08:36:17 +0530

    adding new book and get book details by id

--- library-management/src/main/java/com/hexad/library/management/service/BookServiceImpl.java
+++ library-management/src/main/java/com/hexad/library/management/service/BookServiceImpl.java
@@ -2,6 +2,7 @@ package com.hexad.library.management.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -20,15 +21,28 @@ public class BookServiceImpl implements BookService {
 
 	@Override
 	public Book getbook(String bookId) throws BookNotFoundException {
-		
-		return null;
+		Optional<Book> bookOptional = this.bookList.stream().filter(id -> bookId.equals(bookId)).findAny();
+
+		if (!bookOptional.isPresent())
+			throw new BookNotFoundException("Book Does not exists");
+
+		return bookOptional.get();
 	}
 
 	@Override
 	public Response addbook(Book book) {
 
+		Optional<Book> bookOptional = bookList.stream().filter(id -> id.getId().equals(book.getId())).findAny();
 		Response response = new Response();
-		
+		response.setId(book.getId());
+		if(bookOptional.isPresent()) {
+			int indexOf = this.bookList.indexOf(bookOptional.get());
+			bookList.set(indexOf, book);
+			response.setMessage("Updated succesfully!");
+		}else {
+			this.bookList.add(book);
+			response.setMessage("Saved succesfully!");
+		}
 		return response;
 	}
 
--- library-management/src/test/java/com/hexad/library/managemnent/service/BookServiceImplTest.java
+++ library-management/src/test/java/com/hexad/library/managemnent/service/BookServiceImplTest.java
@@ -1,5 +1,7 @@
 package com.hexad.library.managemnent.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +11,8 @@ import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.hexad.library.management.exception.BookNotFoundException;
+import com.hexad.library.management.model.Book;
+import com.hexad.library.management.model.Response;
 import com.hexad.library.management.service.BookServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,11 +35,21 @@ public class BookServiceImplTest {
 
 	@Test
 	public void addBook_WhenNoBooksPresent() throws BookNotFoundException {
-
+		String bookId = "123";
+		Book book = Book.builder().id(bookId).isbn("ISBN123").title("System Design").build();
+		Response response = bookService.addbook(book);
+		assertEquals(bookId, response.getId());
+		assertNotNull(bookService.getbook(bookId));
 	}
-
+	
 	@Test
 	public void updateBook_WhenBookPresent() throws BookNotFoundException {
-
+		String bookId = "123";
+		Book book = Book.builder().id(bookId).isbn("ISBN123").title("System Design").build();
+		bookService.addbook(book);
+		assertEquals("ISBN123", bookService.getbook(bookId).getIsbn());
+		book.setIsbn("ISBN321");
+		bookService.addbook(book);
+		assertEquals("ISBN321", bookService.getbook(bookId).getIsbn());
 	}
 }
